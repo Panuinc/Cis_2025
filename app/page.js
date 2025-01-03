d21ba046ffb867b3f61018cf6350509a6077d2ee
Panuinc <panuwat.ja.forwork@gmail.com>
2025-01-04 00:25:12 +0700

    update

--- app/page.js
+++ app/page.js
@@ -3,10 +3,10 @@ import React from "react";
 export default function page() {
   return (
     <>
-      <div className="flex items-center justify-center w-full h-20 p-2 gap-2 bg-white rounded-full text-success">
+      <div className="flex items-center justify-center w-full h-20 p-2 gap-2 border-2 border-dark border-dashed bg-white rounded-full text-success">
         Whereas disregard สวัสดี
       </div>
-      <div className="flex items-center justify-center w-full h-full p-2 gap-2 bg-white rounded-3xl">
+      <div className="flex items-center justify-center w-full h-full p-2 gap-2 border-2 border-dark border-dashed bg-white rounded-3xl">
         01
       </div>
     </>
