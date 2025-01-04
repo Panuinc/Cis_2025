8ed3084c222fdcc1c9dfdb6cb492e8a789be5652
Panuinc <panuwat.ja.forwork@gmail.com>
2025-01-04 15:34:00 +0700

    update

--- app/page.js
+++ app/page.js
@@ -3,12 +3,6 @@ import React from "react";
 export default function page() {
   return (
     <>
-      <div className="flex items-center justify-center w-full h-20 p-2 gap-2 border-2 border-dark border-dashed bg-white rounded-full text-success">
-        Whereas disregard สวัสดี
-      </div>
-      <div className="flex items-center justify-center w-full h-full p-2 gap-2 border-2 border-dark border-dashed bg-white rounded-3xl">
-        01
-      </div>
     </>
   );
 }
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,12 @@
+import { nextui } from "@nextui-org/theme";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
@@ -24,5 +27,6 @@ export default {
       },
     },
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [nextui()],
 };
