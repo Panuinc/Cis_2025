c2bfdb50e5fc6a627acd508a9ff10caa20f94209
Panuwat IINC <panuwat.ja.forwork@gmail.com>
2025-01-06 09:34:25 +0700

    update

--- app/(ui)/(hr)/branch/page.js
+++ app/(ui)/(hr)/branch/page.js
@@ -1,6 +1,9 @@
+"use client";
 import TopicHeader from "@/components/form/TopicHeader";
 import Link from "next/link";
 import React from "react";
+import { Input, Button } from "@nextui-org/react";
+import { Add, Search } from "@/components/icons/icons";
 
 export default function BranchPage() {
   return (
@@ -9,21 +12,32 @@ export default function BranchPage() {
       <div className="flex flex-col items-center justify-center w-full h-full p-2 gap-2 border-2 border-dark border-dashed">
         <div className="flex flex-row items-center justify-center w-full h-full p-2 gap-2 border-2 border-dark border-dashed">
           <div className="flex items-center justify-center w-full h-full p-2 gap-2 border-2 border-dark border-dashed">
-            Search
+            <Input
+              type="text"
+              placeholder="Search"
+              size="lg"
+              variant="bordered"
+              startContent={<Search />}
+              // value={userUsername}
+              // onChange={(e) => setUserUsername(e.target.value)}
+              // isRequired={true}
+            />
           </div>
           <Link
             href="/branch/create"
-            className="flex items-center justify-center w-full h-full p-2 gap-2 border-2 border-dark border-dashed"
+            className="flex items-center justify-center h-full p-2 gap-2 border-2 border-dark border-dashed"
           >
-            Add New
+            <Button size="lg" color="primary">
+              <Add /> Add New
+            </Button>
           </Link>
         </div>
         <div className="flex flex-col items-center justify-center w-full h-full p-2 gap-2 border-2 border-dark border-dashed">
           <div className="flex flex-row items-center justify-center w-full h-full p-2 gap-2 border-2 border-dark border-dashed">
-            <div className="flex items-center justify-center w-full h-full p-2 gap-2 border-2 border-dark border-dashed">
+            <div className="flex items-center justify-start w-full h-full p-2 gap-2 border-2 border-dark border-dashed">
               Total Row
             </div>
-            <div className="flex items-center justify-center w-full h-full p-2 gap-2 border-2 border-dark border-dashed">
+            <div className="flex items-center justify-end w-full h-full p-2 gap-2 border-2 border-dark border-dashed">
               Select Row Show
             </div>
           </div>
@@ -83,10 +97,10 @@ export default function BranchPage() {
             </div>
           </div>
           <div className="flex flex-row items-center justify-center w-full h-full p-2 gap-2 border-2 border-dark border-dashed">
-            <div className="flex items-center justify-center w-full h-full p-2 gap-2 border-2 border-dark border-dashed">
+            <div className="flex items-center justify-start w-full h-full p-2 gap-2 border-2 border-dark border-dashed">
               xxx Row Form xxx Row
             </div>
-            <div className="flex items-center justify-center w-full h-full p-2 gap-2 border-2 border-dark border-dashed">
+            <div className="flex items-center justify-end w-full h-full p-2 gap-2 border-2 border-dark border-dashed">
               Paginations
             </div>
           </div>
