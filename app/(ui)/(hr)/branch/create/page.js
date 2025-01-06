95869a43349b6f58c0ac335f387a3969c79c5dfd
Panuwat IINC <panuwat.ja.forwork@gmail.com>
2025-01-06 09:07:53 +0700

    update

--- app/(ui)/(hr)/branch/create/page.js
+++ app/(ui)/(hr)/branch/create/page.js
@@ -1,70 +1,13 @@
 "use client";
 import React from "react";
 import TopicHeader from "@/components/form/TopicHeader";
-import { Input, Button } from "@nextui-org/react";
-import { Cancel, Database } from "@/components/icons/icons";
+import FormBranch from "@/components/form/hr/branch/FormBranch";
 
 export default function BranchCreate() {
   return (
     <>
       <TopicHeader topic="BranchCreate" />
-      <div className="flex flex-col items-center justify-center w-full h-full p-2 gap-2 border-2 border-dark border-dashed">
-        <div className="flex flex-col xl:flex-row items-center justify-center w-full h-full p-2 gap-2 border-2 border-dark border-dashed">
-          <div className="flex items-center justify-center w-full h-full p-2 gap-2 border-2 border-dark border-dashed">
-            <Input
-              type="text"
-              label="Text"
-              placeholder="Please Enter Data"
-              labelPlacement="outside"
-              size="lg"
-              variant="bordered"
-              // value={userUsername}
-              // onChange={(e) => setUserUsername(e.target.value)}
-              // isRequired={true}
-            />
-          </div>
-          <div className="flex items-center justify-center w-full h-full p-2 gap-2 border-2 border-dark border-dashed">
-            <Input
-              type="text"
-              label="Text"
-              placeholder="Please Enter Data"
-              labelPlacement="outside"
-              size="lg"
-              variant="bordered"
-              // value={userUsername}
-              // onChange={(e) => setUserUsername(e.target.value)}
-              // isRequired={true}
-            />
-          </div>
-        </div>
-        <div className="flex flex-col xl:flex-row items-center justify-center w-full h-full p-2 gap-2 border-2 border-dark border-dashed">
-          <div className="flex items-center justify-center w-full h-full p-2 gap-2 border-2 border-dark border-dashed">
-            <Input
-              type="text"
-              label="Text"
-              placeholder="Please Enter Data"
-              labelPlacement="outside"
-              size="lg"
-              variant="bordered"
-              // value={userUsername}
-              // onChange={(e) => setUserUsername(e.target.value)}
-              // isRequired={true}
-            />
-          </div>
-        </div>
-        <div className="flex xl:flex-row items-center justify-end w-full h-full p-2 gap-2 border-2 border-dark border-dashed">
-          <div className="flex items-center justify-center h-full p-2 gap-2 border-2 border-dark border-dashed">
-            <Button size="md" color="success" type="submit">
-              <Database /> Submit
-            </Button>
-          </div>
-          <div className="flex items-center justify-center h-full p-2 gap-2 border-2 border-dark border-dashed">
-            <Button size="md" color="danger" type="submit">
-              <Cancel /> Cancel
-            </Button>
-          </div>
-        </div>
-      </div>
+      <FormBranch />
     </>
   );
 }
