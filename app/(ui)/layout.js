dd09726a558e19363f6502addf78e69172998879
Panuwat IINC <panuwat.ja.forwork@gmail.com>
2025-01-07 14:10:02 +0700

    update

--- app/(ui)/layout.js
+++ app/(ui)/layout.js
@@ -7,6 +7,7 @@ import Link from "next/link";
 import { useSession, signOut } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import Loading from "@/components/Loading";
+import toast, { Toaster } from "react-hot-toast";
 import {
   Ac,
   Bell,
@@ -267,127 +268,133 @@ export default function UiLayout({ children }) {
     return null;
   }
   return (
-    <div className="flex flex-col items-center justify-center w-full h-full p-2 gap-2 border-2 border-dark border-dashed">
-      <div className="flex flex-row items-center justify-between w-full h-20 p-2 gap-4 border-2 border-dark border-dashed">
-        <div
-          className="xl:hidden flex items-center justify-center h-full p-2 gap-2 border-2 border-dark border-dashed bg-white rounded-full"
-          onClick={handleOpenMobileMenu}
-        >
-          <div className="flex items-center justify-center w-full h-full p-2 gap-2 border-2 border-dark border-dashed">
-            <Cis />
-          </div>
-          <div className="flex items-center justify-center w-full h-full p-2 gap-2 border-2 border-dark border-dashed font-[600] text-xl text-success">
-            CIS
-          </div>
-        </div>
-        <div className="hidden xl:flex flex-row items-center justify-center h-full px-4 py-2 gap-2 border-2 border-dark border-dashed bg-white rounded-full">
-          <div className="flex items-center justify-center w-full h-full p-2 gap-2 border-2 border-dark border-dashed">
-            <Cis />
-          </div>
-          <div className="flex items-center justify-center w-full h-full p-2 gap-2 border-2 border-dark border-dashed font-[600] text-xl text-success">
-            CIS
-          </div>
-        </div>
-        <div className="hidden xl:flex flex-row items-center justify-between w-full h-full px-4 py-2 gap-2 border-2 border-dark border-dashed  bg-white rounded-full">
-          <MenuHeaderHide icons={<Hidden />} onClick={toggleHeaderMenu} />
-          <MenuHeader
-            href="https://channakorn.co.th/"
-            icons={<Company />}
-            text="Cne"
-            target="_blank"
-          />
-          <MenuHeader
-            href="http://49.0.66.19:8023/Main/"
-            icons={<CneSystem />}
-            text="System"
-            target="_blank"
-          />
-          <MenuHeader
-            href="http://cnecloud01.myqnapcloud.com:8011/"
-            icons={<CneCloud />}
-            text="Cloud"
-            target="_blank"
-          />
-          <MenuHeader
-            href="http://49.0.64.242:8088/LoginERS/login.aspx"
-            icons={<LeaveWork />}
-            text="Day Off"
-            target="_blank"
-          />
-          <MenuHeader
-            href="/logo"
-            icons={<Logo />}
-            text="Logo"
-            target="_blank"
-          />
-          <MenuHeader
-            href="/contact"
-            icons={<Contact />}
-            text="Contact"
-            target="_blank"
-          />
-        </div>
-        <div className="xl:hidden flex flex-col relative w-full h-full p-2 gap-2 border-2 border-dark border-dashed bg-white  rounded-full">
-          <MenuHeaderHide icons={<Hidden />} onClick={toggleHeaderMenu} />
-          {isMobileHeaderOpen && (
-            <div
-              className="absolute top-[100%] right-0 flex flex-col items-center justify-center w-[256px] p-2 mt-[20px] gap-2 border-2 border-dark border-dashed bg-white  z-50"
-              ref={mobileHeaderRef}
-            >
-              <MenuHeader
-                href="https://channakorn.co.th/"
-                icons={<Company />}
-                text="Cne"
-              />
-              <MenuHeader
-                href="http://49.0.66.19:8023/Main/"
-                icons={<CneSystem />}
-                text="System"
-              />
-              <MenuHeader
-                href="http://cnecloud01.myqnapcloud.com:8011/"
-                icons={<CneCloud />}
-                text="Cloud"
-              />
-              <MenuHeader
-                href="http://49.0.64.242:8088/LoginERS/login.aspx"
-                icons={<LeaveWork />}
-                text="Day Off"
-              />
-              <MenuHeader href="/logo" icons={<Logo />} text="Logo" />
-              <MenuHeader href="/contact" icons={<Contact />} text="Contact" />
+    <>
+      <Toaster position="top-right" />
+      <div className="flex flex-col items-center justify-center w-full h-full p-2 gap-2 border-2 border-dark border-dashed">
+        <div className="flex flex-row items-center justify-between w-full h-20 p-2 gap-4 border-2 border-dark border-dashed">
+          <div
+            className="xl:hidden flex items-center justify-center h-full p-2 gap-2 border-2 border-dark border-dashed bg-white rounded-full"
+            onClick={handleOpenMobileMenu}
+          >
+            <div className="flex items-center justify-center w-full h-full p-2 gap-2 border-2 border-dark border-dashed">
+              <Cis />
             </div>
-          )}
-        </div>
-        <div className="flex flex-row items-center justify-center h-full p-2 xl:px-4 xl:py-2 gap-2 border-2 border-dark border-dashed bg-white rounded-full">
-          <div className="xl:flex hidden items-center justify-center w-60 h-full p-2 gap-2 border-2 border-dark border-dashed">
-            <Input
-              type="text"
-              placeholder="Search"
-              size="sm"
-              radius="full"
-              variant="bordered"
-              startContent={<Search />}
+            <div className="flex items-center justify-center w-full h-full p-2 gap-2 border-2 border-dark border-dashed font-[600] text-xl text-success">
+              CIS
+            </div>
+          </div>
+          <div className="hidden xl:flex flex-row items-center justify-center h-full px-4 py-2 gap-2 border-2 border-dark border-dashed bg-white rounded-full">
+            <div className="flex items-center justify-center w-full h-full p-2 gap-2 border-2 border-dark border-dashed">
+              <Cis />
+            </div>
+            <div className="flex items-center justify-center w-full h-full p-2 gap-2 border-2 border-dark border-dashed font-[600] text-xl text-success">
+              CIS
+            </div>
+          </div>
+          <div className="hidden xl:flex flex-row items-center justify-between w-full h-full px-4 py-2 gap-2 border-2 border-dark border-dashed  bg-white rounded-full">
+            <MenuHeaderHide icons={<Hidden />} onClick={toggleHeaderMenu} />
+            <MenuHeader
+              href="https://channakorn.co.th/"
+              icons={<Company />}
+              text="Cne"
+              target="_blank"
+            />
+            <MenuHeader
+              href="http://49.0.66.19:8023/Main/"
+              icons={<CneSystem />}
+              text="System"
+              target="_blank"
+            />
+            <MenuHeader
+              href="http://cnecloud01.myqnapcloud.com:8011/"
+              icons={<CneCloud />}
+              text="Cloud"
+              target="_blank"
+            />
+            <MenuHeader
+              href="http://49.0.64.242:8088/LoginERS/login.aspx"
+              icons={<LeaveWork />}
+              text="Day Off"
+              target="_blank"
+            />
+            <MenuHeader
+              href="/logo"
+              icons={<Logo />}
+              text="Logo"
+              target="_blank"
+            />
+            <MenuHeader
+              href="/contact"
+              icons={<Contact />}
+              text="Contact"
+              target="_blank"
             />
           </div>
-          <div className="flex items-center justify-center w-10 h-10 p-2 gap-2 border-2 border-dark border-dashed bg-default rounded-full">
-            <Bell />
+          <div className="xl:hidden flex flex-col relative w-full h-full p-2 gap-2 border-2 border-dark border-dashed bg-white  rounded-full">
+            <MenuHeaderHide icons={<Hidden />} onClick={toggleHeaderMenu} />
+            {isMobileHeaderOpen && (
+              <div
+                className="absolute top-[100%] right-0 flex flex-col items-center justify-center w-[256px] p-2 mt-[20px] gap-2 border-2 border-dark border-dashed bg-white  z-50"
+                ref={mobileHeaderRef}
+              >
+                <MenuHeader
+                  href="https://channakorn.co.th/"
+                  icons={<Company />}
+                  text="Cne"
+                />
+                <MenuHeader
+                  href="http://49.0.66.19:8023/Main/"
+                  icons={<CneSystem />}
+                  text="System"
+                />
+                <MenuHeader
+                  href="http://cnecloud01.myqnapcloud.com:8011/"
+                  icons={<CneCloud />}
+                  text="Cloud"
+                />
+                <MenuHeader
+                  href="http://49.0.64.242:8088/LoginERS/login.aspx"
+                  icons={<LeaveWork />}
+                  text="Day Off"
+                />
+                <MenuHeader href="/logo" icons={<Logo />} text="Logo" />
+                <MenuHeader
+                  href="/contact"
+                  icons={<Contact />}
+                  text="Contact"
+                />
+              </div>
+            )}
           </div>
-          <div className="flex items-center justify-center w-10 h-10 p-2 gap-2 border-2 border-dark border-dashed bg-success rounded-full">
-            <Image
-              width={25}
-              height={25}
-              src="/images/picture/test.png"
-              alt="test"
-              priority={true}
-            />
+          <div className="flex flex-row items-center justify-center h-full p-2 xl:px-4 xl:py-2 gap-2 border-2 border-dark border-dashed bg-white rounded-full">
+            <div className="xl:flex hidden items-center justify-center w-60 h-full p-2 gap-2 border-2 border-dark border-dashed">
+              <Input
+                type="text"
+                placeholder="Search"
+                size="sm"
+                radius="full"
+                variant="bordered"
+                startContent={<Search />}
+              />
+            </div>
+            <div className="flex items-center justify-center w-10 h-10 p-2 gap-2 border-2 border-dark border-dashed bg-default rounded-full">
+              <Bell />
+            </div>
+            <div className="flex items-center justify-center w-10 h-10 p-2 gap-2 border-2 border-dark border-dashed bg-success rounded-full">
+              <Image
+                width={25}
+                height={25}
+                src="/images/picture/test.png"
+                alt="test"
+                priority={true}
+              />
+            </div>
           </div>
         </div>
-      </div>
-      <div className="flex flex-row items-start justify-center w-full h-full p-2 gap-6 border-2 border-dark border-dashed overflow-auto">
-        <div
-          ref={sideMenuRef}
-          className={`
+        <div className="flex flex-row items-start justify-center w-full h-full p-2 gap-6 border-2 border-dark border-dashed overflow-auto">
+          <div
+            ref={sideMenuRef}
+            className={`
             ${isMobileMenuOpen ? "flex" : "hidden"}
             xl:flex 
             flex-col items-center justify-between
@@ -397,103 +404,103 @@ export default function UiLayout({ children }) {
             overflow-auto bg-white rounded-3xl fixed xl:static
             top-0 left-0 z-50
           `}
-        >
-          <MenuMainOther
-            icons={<Hide />}
-            text="Channakorn Engineer"
-            isCollapsed={isCollapsed}
-            onClick={handleToggleMenu}
-          />
-          <div className="flex flex-col items-center justify-start w-full h-full p-2 gap-2 border-2 border-dark border-dashed  overflow-auto">
-            <MenuMain
-              icons={<Dashboard />}
-              text="Dashboard"
+          >
+            <MenuMainOther
+              icons={<Hide />}
+              text="Channakorn Engineer"
               isCollapsed={isCollapsed}
-              options={[{ label: "Dashboard", href: "/dashboard" }]}
-              isOpen={menuState.Dashboard}
-              onToggle={() => toggleMenu("Dashboard")}
+              onClick={handleToggleMenu}
             />
-            <MenuMain
-              icons={<Hr />}
-              text="HR Department"
+            <div className="flex flex-col items-center justify-start w-full h-full p-2 gap-2 border-2 border-dark border-dashed  overflow-auto">
+              <MenuMain
+                icons={<Dashboard />}
+                text="Dashboard"
+                isCollapsed={isCollapsed}
+                options={[{ label: "Dashboard", href: "/dashboard" }]}
+                isOpen={menuState.Dashboard}
+                onToggle={() => toggleMenu("Dashboard")}
+              />
+              <MenuMain
+                icons={<Hr />}
+                text="HR Department"
+                isCollapsed={isCollapsed}
+                options={[
+                  { label: "Branch", href: "/branch" },
+                  { label: "Site", href: "/site" },
+                  { label: "Division", href: "/division" },
+                ]}
+                isOpen={menuState.HR}
+                onToggle={() => toggleMenu("HR")}
+              />
+              <MenuMain
+                icons={<IT />}
+                text="IT Department"
+                isCollapsed={isCollapsed}
+                options={[
+                  { label: "Backup", href: "/backup" },
+                  { label: "Network", href: "/network" },
+                ]}
+                isOpen={menuState.IT}
+                onToggle={() => toggleMenu("IT")}
+              />
+              <MenuMain
+                icons={<Ac />}
+                text="AC Department"
+                isCollapsed={isCollapsed}
+                options={[
+                  { label: "Option A", href: "/OptionA" },
+                  { label: "Option B", href: "/OptionB" },
+                ]}
+                isOpen={menuState.AC}
+                onToggle={() => toggleMenu("AC")}
+              />
+              <MenuMain
+                icons={<Pu />}
+                text="PU Department"
+                isCollapsed={isCollapsed}
+                options={[
+                  { label: "Option C", href: "/OptionC" },
+                  { label: "Option D", href: "/OptionD" },
+                ]}
+                isOpen={menuState.PU}
+                onToggle={() => toggleMenu("PU")}
+              />
+              <MenuMain
+                icons={<En />}
+                text="EN Department"
+                isCollapsed={isCollapsed}
+                options={[
+                  { label: "Option E", href: "/OptionE" },
+                  { label: "Option F", href: "/OptionF" },
+                ]}
+                isOpen={menuState.EN}
+                onToggle={() => toggleMenu("EN")}
+              />
+              <MenuMain
+                icons={<Setting />}
+                text="Setting"
+                isCollapsed={isCollapsed}
+                options={[
+                  { label: "Option G", href: "/OptionG" },
+                  { label: "Option H", href: "/OptionH" },
+                ]}
+                isOpen={menuState.SETTING}
+                onToggle={() => toggleMenu("SETTING")}
+              />
+            </div>
+            <MenuMainOther
+              icons={<Logout />}
+              text="Logout"
               isCollapsed={isCollapsed}
-              options={[
-                { label: "Branch", href: "/branch" },
-                { label: "Site", href: "/site" },
-                { label: "Division", href: "/division" },
-              ]}
-              isOpen={menuState.HR}
-              onToggle={() => toggleMenu("HR")}
-            />
-            <MenuMain
-              icons={<IT />}
-              text="IT Department"
-              isCollapsed={isCollapsed}
-              options={[
-                { label: "Backup", href: "/backup" },
-                { label: "Network", href: "/network" },
-              ]}
-              isOpen={menuState.IT}
-              onToggle={() => toggleMenu("IT")}
-            />
-            <MenuMain
-              icons={<Ac />}
-              text="AC Department"
-              isCollapsed={isCollapsed}
-              options={[
-                { label: "Option A", href: "/OptionA" },
-                { label: "Option B", href: "/OptionB" },
-              ]}
-              isOpen={menuState.AC}
-              onToggle={() => toggleMenu("AC")}
-            />
-            <MenuMain
-              icons={<Pu />}
-              text="PU Department"
-              isCollapsed={isCollapsed}
-              options={[
-                { label: "Option C", href: "/OptionC" },
-                { label: "Option D", href: "/OptionD" },
-              ]}
-              isOpen={menuState.PU}
-              onToggle={() => toggleMenu("PU")}
-            />
-            <MenuMain
-              icons={<En />}
-              text="EN Department"
-              isCollapsed={isCollapsed}
-              options={[
-                { label: "Option E", href: "/OptionE" },
-                { label: "Option F", href: "/OptionF" },
-              ]}
-              isOpen={menuState.EN}
-              onToggle={() => toggleMenu("EN")}
-            />
-            <MenuMain
-              icons={<Setting />}
-              text="Setting"
-              isCollapsed={isCollapsed}
-              options={[
-                { label: "Option G", href: "/OptionG" },
-                { label: "Option H", href: "/OptionH" },
-              ]}
-              isOpen={menuState.SETTING}
-              onToggle={() => toggleMenu("SETTING")}
+              onClick={() => {
+                signOut({
+                  callbackUrl: "/",
+                });
+              }}
             />
           </div>
-          <MenuMainOther
-            icons={<Logout />}
-            text="Logout"
-            isCollapsed={isCollapsed}
-            onClick={() => {
-              signOut({
-                callbackUrl: "/",
-              });
-            }}
-          />
-        </div>
-        <div
-          className={`
+          <div
+            className={`
             flex flex-col items-center justify-between
             ${!isMobileMenuOpen ? "w-[100%]" : "w-[100%]"}
             xl:${isCollapsed ? "w-[90%]" : "w-[75%]"}
@@ -501,12 +508,13 @@ export default function UiLayout({ children }) {
             overflow-auto bg-white rounded-3xl
             transition-all
           `}
-        >
-          <div className="flex flex-col items-center justify-center w-full p-2 gap-2 border-2 border-dark border-dashed ">
-            {children}
+          >
+            <div className="flex flex-col items-center justify-center w-full p-2 gap-2 border-2 border-dark border-dashed ">
+              {children}
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
