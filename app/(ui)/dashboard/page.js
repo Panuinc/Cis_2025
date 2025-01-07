129d381e3cf3106b0a5cb2380cea7b86d993b998
Panuwat IINC <panuwat.ja.forwork@gmail.com>
2025-01-07 14:41:17 +0700

    update

--- app/(ui)/dashboard/page.js
+++ app/(ui)/dashboard/page.js
@@ -1,3 +1,4 @@
+import FormDashboard from "@/components/form/dashboard/FormDashboard";
 import TopicHeader from "@/components/form/TopicHeader";
 import React from "react";
 
@@ -5,9 +6,7 @@ export default function Dashboard() {
   return (
     <>
       <TopicHeader topic="Dashboard" />
-      <div className="flex flex-col items-center justify-center w-full h-full p-2 gap-2 border-2 border-dark border-dashed">
-        Contents
-      </div>
+      <FormDashboard />
     </>
   );
 }
