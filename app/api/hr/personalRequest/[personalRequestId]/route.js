bd2954cfba0c850f36513d19f8112ef6e78d9956
Panuwat IINC <panuwat.ja.forwork@gmail.com>
2025-01-15 14:30:03 +0700

    update

--- app/api/hr/personalRequest/[personalRequestId]/route.js
+++ app/api/hr/personalRequest/[personalRequestId]/route.js
@@ -1,6 +1,11 @@
 import { NextResponse } from "next/server";
 import { handleErrors, handleGetErrors } from "@/lib/errorHandler";
-import { personalRequestPutSchema } from "@/app/api/hr/personalRequest/personalRequestSchema";
+import {
+  personalRequestPutSchema,
+  personalRequestManagerApprovePutSchema,
+  personalRequestHrApprovePutSchema,
+  personalRequestMdApprovePutSchema,
+} from "@/app/api/hr/personalRequest/personalRequestSchema";
 import { verifySecretToken } from "@/lib/auth";
 import { checkRateLimit } from "@/lib/rateLimit";
 import prisma from "@/lib/prisma";
@@ -73,30 +78,88 @@ export async function PUT(request, context) {
       );
     }
 
+    const { searchParams } = new URL(request.url);
+    const action = searchParams.get("action");
+    if (!action) {
+      return NextResponse.json(
+        { error: "Query parameter 'action' is required" },
+        { status: 400 }
+      );
+    }
+
     verifySecretToken(request.headers);
     await checkRateLimit(ip);
 
     const formData = await request.formData();
     const data = Object.fromEntries(formData);
-
-    const parsedData = personalRequestPutSchema.parse({
-      ...data,
-      personalRequestId,
-      personalRequestDesiredDate: new Date(data.personalRequestDesiredDate),
-    });
-
     const localNow = getLocalNow();
+
+    let parsedData, updateData, message;
+
+    switch (action) {
+      case "update":
+        parsedData = personalRequestPutSchema.parse({
+          ...data,
+          personalRequestId,
+          personalRequestDesiredDate: new Date(data.personalRequestDesiredDate),
+        });
+        updateData = {
+          ...parsedData,
+          personalRequestUpdateAt: localNow,
+        };
+        message = "PersonalRequest data updated successfully";
+        break;
+
+      case "managerApprove":
+        parsedData = personalRequestManagerApprovePutSchema.parse({
+          ...data,
+          personalRequestId,
+        });
+        updateData = {
+          ...parsedData,
+          personalRequestReasonManagerApproveAt: localNow,
+        };
+        message = "PersonalRequest data updated By Manager successfully";
+        break;
+
+      case "hrApprove":
+        parsedData = personalRequestHrApprovePutSchema.parse({
+          ...data,
+          personalRequestId,
+        });
+        updateData = {
+          ...parsedData,
+          personalRequestReasonHrApproveAt: localNow,
+        };
+        message = "PersonalRequest data updated By Hr successfully";
+        break;
+
+      case "mdApprove":
+        parsedData = personalRequestMdApprovePutSchema.parse({
+          ...data,
+          personalRequestId,
+        });
+        updateData = {
+          ...parsedData,
+          personalRequestReasonMdApproveAt: localNow,
+        };
+        message = "PersonalRequest data updated By Md successfully";
+        break;
+
+      default:
+        return NextResponse.json({ error: "Invalid action" }, { status: 400 });
+    }
 
     const updatedPersonalRequest = await prisma.personalRequest.update({
       where: { personalRequestId: parseInt(personalRequestId, 10) },
-      data: {
-        ...parsedData,
-        personalRequestUpdateAt: localNow,
-      },
+      data: updateData,
     });
 
     return NextResponse.json(
-      { message: "PersonalRequest data updated successfully", personalRequest: updatedPersonalRequest },
+      {
+        message,
+        personalRequest: updatedPersonalRequest,
+      },
       { status: 200 }
     );
   } catch (error) {
