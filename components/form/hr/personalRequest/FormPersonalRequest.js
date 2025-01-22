dd2ba7cb0846669417f7fdc40d1ae77af2e98266
Panuwat IINC <panuwat.ja.forwork@gmail.com>
2025-01-22 13:52:59 +0700

    update

--- components/form/hr/personalRequest/FormPersonalRequest.js
+++ components/form/hr/personalRequest/FormPersonalRequest.js
@@ -414,7 +414,7 @@ export default function FormPersonalRequest({
         </div>
       </div>
 
-      {isUpdate && (
+      {!isParentOfCreator && isUpdate && (
         <div className="flex flex-col xl:flex-row items-center justify-center w-full h-full p-2 gap-2 border-2 border-dark border-dashed">
           <div className="flex items-center justify-center w-full h-full p-2 gap-2 border-2 border-dark border-dashed">
             <Select
@@ -487,29 +487,31 @@ export default function FormPersonalRequest({
         </div>
       )}
 
-      <div className="flex flex-row items-center justify-end w-full h-full p-2 gap-2 border-2 border-dark border-dashed">
-        <div className="flex items-center justify-center h-full p-2 gap-2 border-2 border-dark border-dashed">
-          <Button
-            size="md"
-            color="success"
-            startContent={<Database />}
-            type="submit"
-          >
-            Submit
-          </Button>
+      {!isParentOfCreator && (
+        <div className="flex flex-row items-center justify-end w-full h-full p-2 gap-2 border-2 border-dark border-dashed">
+          <div className="flex items-center justify-center h-full p-2 gap-2 border-2 border-dark border-dashed">
+            <Button
+              size="md"
+              color="success"
+              startContent={<Database />}
+              type="submit"
+            >
+              Submit
+            </Button>
+          </div>
+          <div className="flex items-center justify-center h-full p-2 gap-2 border-2 border-dark border-dashed">
+            <Button
+              size="md"
+              color="danger"
+              startContent={<Cancel />}
+              onPress={onClear}
+              type="button"
+            >
+              Cancel
+            </Button>
+          </div>
         </div>
-        <div className="flex items-center justify-center h-full p-2 gap-2 border-2 border-dark border-dashed">
-          <Button
-            size="md"
-            color="danger"
-            startContent={<Cancel />}
-            onPress={onClear}
-            type="button"
-          >
-            Cancel
-          </Button>
-        </div>
-      </div>
+      )}
     </form>
   );
 }
